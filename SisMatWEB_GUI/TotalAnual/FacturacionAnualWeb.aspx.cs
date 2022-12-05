094818eac915f6191c10a721321f957534b0fdea
Diego <diego.moram23@gmail.com>
2022-12-05 15:59:12 -0500

    Ingreso de graficos

---
+++ SisMatWEB_GUI/TotalAnual/FacturacionAnualWeb.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SisMat_BL;
+
+namespace SisMatWEB_GUI.TotalAnual
+{
+    public partial class FacturacionAnualWeb : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+			try
+			{
+				if(Page.IsPostBack == false)
+				{
+					FacturacionBL objFacturacionBL = new FacturacionBL();
+					grvFacturaAnual.DataSource= objFacturacionBL.ListarFacturaAnual();
+					grvFacturaAnual.DataBind();
+
+					//grfTotales
+					DataTableReader dtReaderTotales = objFacturacionBL.ListarFacturaAnual().CreateDataReader();
+					grfTotales.Series.Add("Totales");
+					grfTotales.Series["Totales"].Points.DataBindXY(dtReaderTotales,"Año",dtReaderTotales,"total");
+					grfTotales.Series["Totales"].IsValueShownAsLabel= true;
+					grfTotales.Series["Totales"].LabelFormat = "c";
+
+					//grfMatriculados
+					DataTableReader dtReaderMatriculados = objFacturacionBL.ListarFacturaAnual().CreateDataReader();
+                    grfMatriculados.Series.Add("Matriculados");
+                    grfMatriculados.Series["Matriculados"].Points.DataBindXY(dtReaderMatriculados,"Año", dtReaderMatriculados,"CantAlumMatriculados");
+                    grfMatriculados.Series["Matriculados"].IsValueShownAsLabel = true;
+					grfMatriculados.Series["Matriculados"].LabelFormat = "n";
+				}
+			}
+			catch (Exception ex)
+			{
+
+				lblMensaje.Text = ex.Message;
+			}
+        }
+    }
+}
---
+++ SisMat_BL/FacturacionBL.cs
@@ -0,0 +1,20 @@
+﻿using SisMat_ADO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisMat_BL
+{
+    public class FacturacionBL
+    {
+        FacturacionADO objFacturacionADO = new FacturacionADO();
+
+        public DataTable ListarFacturaAnual()
+        {
+            return objFacturacionADO.ListarFacturaAnual();
+        }
+    }
+}
